bad56306031c5ccaf7c3dda6ad518421a146b218
WhAnci <leeshiny7@icloud.com>
2026-03-04 16:54:32 +0900

    Update docker.sh

--- Kakurin Yakudo/_qkstart/scripts/install/docker.sh
+++ Kakurin Yakudo/_qkstart/scripts/install/docker.sh
@@ -13,4 +13,5 @@ docker completion bash > ~/.local/share/bash-completion/completions/docker
 
 mkdir -p ~/.oh-my-zsh/completions
 docker completion zsh > ~/.oh-my-zsh/completions/_docker
+echo "새 쉘에서 docker를 사용하려면 'newgrp docker' 를 사용하세요."
 sudo su $(whoami)
