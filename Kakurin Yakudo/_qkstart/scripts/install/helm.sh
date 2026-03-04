c3c547d0e966623cdcb7cc50c4dad81a2ba29b2d
WhAnci <leeshiny7@icloud.com>
2026-03-04 15:58:36 +0900

    Update helm.sh

--- Kakurin Yakudo/_qkstart/scripts/install/helm.sh
+++ Kakurin Yakudo/_qkstart/scripts/install/helm.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+check_exec curl
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | sh
 
+sudo sh -c 'helm completion bash > /etc/bash_completion.d/helm'
+sudo sh -c "helm completion zsh > \"${fpath[1]}/_helm\""
