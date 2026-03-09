2f0373fa8c921a33c5ff92669a4945ccbfecc53b
WhAnci <leeshiny7@icloud.com>
2026-03-09 09:23:24 +0900

    Create delete-instance-profile.sh

---
+++ Kakurin Yakudo/IAM/Role/delete-instance-profile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ROLE_NAME=KarpenterNodeRole-cluster
+aws iam remove-role-from-instance-profile \
+--instance-profile-name $(aws iam list-instance-profiles-for-role --role-name $ROLE_NAME --query 'InstanceProfiles[].InstanceProfileName' \
+--output json | jq -r '.[]') \
+--role-name $ROLE_NAME
